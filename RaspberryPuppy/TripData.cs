766ac4e92feccf1dabe98edfbe6c0a2a420d9d49
Michael Krüger Andersen <mia004@edu.zealand.dk>
2024-12-05 12:17:45 +0100

    Updated Trip and And personality

--- RaspberryPuppy/TripData.cs
+++ RaspberryPuppy/TripData.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,33 @@ namespace RaspberryPuppy
 {
     public class TripData
     {
+        public TripData()
+        {
+            
+        }
+        public TripData(int id, int tagNr, string? location, string? humidity, int temperature, int timeOutside)
+        {
+            Id = id;
+            TagNr = tagNr;
+            Location = location;
+            Humidity = humidity;
+            Temperature = temperature;
+            TimeOutside = timeOutside;
+        }
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [ForeignKey("Personality")]
+        public int TagNr { get; set; }
+        [Required]
+        public string? Location { get; set; }
+        [Required]
+        public string? Humidity { get; set; }
+        [Required]
+        public int Temperature { get; set; }
+        [Required]
+        public int TimeOutside { get; set; }
     }
 }
